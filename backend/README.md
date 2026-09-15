23f1c36b087e70fc4fc2795c99aa4c7729ac5d69
devch105 <devdhama23@gmail.com>
2026-09-15 13:58:57 +0530

    Initial project setup

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# GitCrawler.io
---
+++ backend/src/test/java/com/git_crawler/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_crawler.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
